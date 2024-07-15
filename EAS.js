4b7144f89470e99c767badc9d4dc9d022ac53a35
Alanah Haynes <alanahihaynes@gmail.com>
2024-07-15 19:15:28 -0400

    Added Working Grid and Instructions

--- EAS.js
+++ EAS.js
@@ -1,11 +1,28 @@
-//Creating 16x16 grid
+/* Create a 16 * 16 Grid */
+const gridContainer=document.querySelector('.grid-container');
 
-const gridContainer= document.querySelector('.grid-container');
-
-let i=0;
-
-for (i=0; i <16; i++){
-    const gridBox= document.createElement('div');
+for (let i=0; i < 16*16; i++){
+    const gridBox=document.createElement('div');
     gridBox.classList.add('grid-box');
     gridContainer.appendChild(gridBox);
+
+    /* Adding Hover Effect Listener */
+    gridBox.addEventListener('mouseover', hoverEffect);
+    
+
+    /* Adding Hover Effects */
+    function hoverEffect(){
+        gridBox.setAttribute('style', 'background-color: white;');
+    }
+
+ 
+
+}
+
+
+const startBtn=document.querySelector('.start-btn');
+startBtn.addEventListener('click', inputSquares);
+
+function inputSquares(){
+    numOfSquares=prompt('Input the number of squares you would like per side. ');
 }
