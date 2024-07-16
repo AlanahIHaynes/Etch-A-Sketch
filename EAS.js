76fc24590d76085b2ec78f3d66599d3a32726b04
Alanah Haynes <alanahihaynes@gmail.com>
2024-07-15 21:40:51 -0400

    Finished initial version of game

--- EAS.js
+++ EAS.js
@@ -1,23 +1,28 @@
-/* Create a 16 * 16 Grid */
 const gridContainer=document.querySelector('.grid-container');
 
-for (let i=0; i < 16*16; i++){
-    const gridBox=document.createElement('div');
-    gridBox.classList.add('grid-box');
-    gridContainer.appendChild(gridBox);
-
-    /* Adding Hover Effect Listener */
-    gridBox.addEventListener('mouseover', hoverEffect);
+function createGrid(numSquares, gridContainer){
+    //clears nodes under grid container
+    gridContainer.innerHTML= '';
+    for (let i=0; i < numSquares*numSquares; i++){
+        const gridBox=document.createElement('div');
+        gridBox.classList.add('grid-box');
+        squareSize= 800 / numSquares;
+        gridBox.style.width= `${squareSize}px`;
+        gridBox.style.height= `${squareSize}px`;
+        gridContainer.appendChild(gridBox);
     
-
-    /* Adding Hover Effects */
-    function hoverEffect(){
-        gridBox.setAttribute('style', 'background-color: white;');
+        /* Adding Hover Effect Listener */
+        gridBox.addEventListener('mouseover', hoverEffect);
+        
+    
+        /* Adding Hover Effects */
+        function hoverEffect(){
+            gridBox.style.backgroundColor='black';
+        }
+    
     }
-
- 
-
 }
+
 
 
 const startBtn=document.querySelector('.start-btn');
@@ -25,4 +30,17 @@ startBtn.addEventListener('click', inputSquares);
 
 function inputSquares(){
     numOfSquares=prompt('Input the number of squares you would like per side. \n(For example, entering 64 creates a 64x64 grid!)');
+    if (numOfSquares < 100 && numOfSquares > 0){
+        createGrid(numOfSquares, gridContainer);
+
+    }
+
+    else{
+        alert('Please enter a number between 1 and 100');
+    }
+
 }
+
+
+
+createGrid(16, gridContainer);
